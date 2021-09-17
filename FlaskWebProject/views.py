b89fe29fb0e2ee925a500aab5670579748ecda7f
Festus Ali <festusali@gmail.com>
2021-09-17 20:02:42 +0100

    Update Redirect URL.

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -136,4 +136,4 @@ def _build_msal_app(cache=None, authority=None):
 
 def _build_auth_url(authority=None, scopes=None, state=None):
     # TODO: Return the full Auth Request URL with appropriate Redirect URI
-    return "http://localhost:5000/getAToken"
+    return "https://flask-cms.azurewebsites.net/.auth/login/aad/callback"
