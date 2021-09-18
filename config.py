acb6523cc8680468426451bd6d491cb0478ee093
Festus Ali <festusali@gmail.com>
2021-09-18 05:04:49 +0100

    Modify connections string.

--- config.py
+++ config.py
@@ -14,8 +14,11 @@ class Config(object):
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'demo-flask'
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'JUST-pass'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
-    SQLALCHEMY_DATABASE_URI = 'mssql+pymssql://' + SQL_USER_NAME + ':' + SQL_PASSWORD + '@' + SQL_SERVER + '/' + SQL_DATABASE
-    'mssql+pyodbc://' + SQL_USER_NAME + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=SQL+Server'
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
+    
+    """SQLALCHEMY_DATABASE_URI = 'mssql+pymssql://' + SQL_USER_NAME + ':' + SQL_PASSWORD + '@' + SQL_SERVER + '/' + SQL_DATABASE
+    'mssql+pyodbc://' + SQL_USER_NAME + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=SQL+Server'"""
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     ### Info for MS Authentication ###
